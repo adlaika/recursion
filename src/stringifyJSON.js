130084b3576af9fecb5ae43147844df8e8f5c0eb
m-arnold <michaelaarnold@gmail.com>
2015-03-30 18:21:11 -0700

    implemented stringifyJSON for all keyless cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  var result = '';
+
+  if (obj === null) {
+    result = 'null';
+  } else if (obj.toString() === [].toString()) {
+    result = '[]';
+  } else if (typeof obj !== 'object') {
+    if (typeof obj === 'string') {
+      result = '"' + obj + '"';
+    } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+      result = obj.toString();
+    }
+  }
+
+  // for (var key in obj) {
+  //   if (obj[key] === undefined || obj[key] instanceof Function) {
+  //     result = result;
+  //   } else
+  //   if (typeof obj[key] === 'boolean' || typeof obj[key] === 'number' || typeof obj[key] === 'string') {
+  //     console.log('boop');
+  //     result += key.toString() + obj[key].toString();
+  //   } else if (obj[key] === null) {
+  //     result += key.toString() + 'null';
+  //   } else {
+  //     result += stringifyJSON(obj[key]);
+  //   }
+  // }
+  console.log('result: ' + result);
+  return result;
 };
