2a030d5ea72c775961b29cf65b7112b08bc2ff01
m-arnold <michaelaarnold@gmail.com>
2015-04-06 15:13:08 -0700

    implemented getElementsByClassName.js such that it passes test.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className) {
+  var elemsWithClass = [];
+  //  domWalk will look at current node and recursively hit every branch,
+  //  checking for className and adding those elems to "elemsWithClass"
+  var domWalk = function (currentNode) {
+    if ($(currentNode).hasClass(className)) {
+      elemsWithClass.push(currentNode);
+    }
+    currentNode = currentNode.firstChild;
+    while (currentNode) {
+      domWalk(currentNode);
+      currentNode = currentNode.nextSibling;
+    }
+  };
+  domWalk(document.body);
+  return elemsWithClass;
 };
+
+console.log('results: ' + getElementsByClassName('targetClassName'));
